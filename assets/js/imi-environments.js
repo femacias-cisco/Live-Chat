dce1d0f74b5b5beb9366db08f12a36ae608605bf
Felipe Macias <femacias@cisco.com>
2026-03-26 11:26:21 -0500

    Initial commit - Webex Connect live chat integration

---
+++ assets/js/imi-environments.js
@@ -0,0 +1,32 @@
+var imiEnvironments = {
+    "target": "default",
+    "default": {
+        "asset": {
+            "appId": 'FE26143154',
+            "appSecret": 'G0VlquD0',
+            "pathConfig": {
+                "assetPath": "/assets/",
+                "root": "/"
+            }
+        },
+        "imiclient": {
+            "shouldRequestNotificationPermission": true,
+            "config": {
+                
+            },
+            "imipush": {
+                "safariWebPushId": 'web.com.imiconnect.safari.webpush'
+            },
+            "authdomain": 'https://rtm-us.imiconnect.io/rtmsAPI',
+            "rtmsdomain": 'rtm-us.imiconnect.io',
+            "safariRegisterURL": 'https://rtm-us.imiconnect.io/apnpweb/'
+        },
+        "sw": {
+            "config": {
+                "messagingSenderId": '',
+                "serverUrl": 'https://rtm-us.imiconnect.io',
+                "appid": 'FE26143154'
+            }
+        }
+    }
+}
